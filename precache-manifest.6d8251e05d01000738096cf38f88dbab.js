0da611b175d6b286729119b0d628dfd9b1c4f067
Max <max@dietr.net>
2020-06-04 11:31:59 +0200

    Updates

---
+++ precache-manifest.6d8251e05d01000738096cf38f88dbab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9610558605b08cb507b4b526bea9b276",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "1fffcf82f0edf14e1e02",
+    "url": "/todolist/static/css/main.ae38e689.chunk.css"
+  },
+  {
+    "revision": "dc9a184751ffbbc05524",
+    "url": "/todolist/static/js/2.482f32d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todolist/static/js/2.482f32d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fffcf82f0edf14e1e02",
+    "url": "/todolist/static/js/main.d2645842.chunk.js"
+  },
+  {
+    "revision": "94e2713e27bcde79676a",
+    "url": "/todolist/static/js/runtime-main.86e7f888.js"
+  }
+]);
